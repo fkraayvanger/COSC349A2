eef204dfa5f42772a4af187506e58e2b684973d4
fkraayvanger <89968563+fkraayvanger@users.noreply.github.com>
2021-10-13 16:01:21 +1300

    SQL Query file for RDS

---
+++ dog_database.sql
@@ -0,0 +1,28 @@
+CREATE TABLE dog_options {
+       dog_name VARCHAR(10),
+       dog_price DECIMAL(13, 2),
+       dog_age INT,
+       dog_breed VARCHAR(25),
+       dog_sex CHAR(1),
+       dog_description VARCHAR(255)
+);
+
+
+INSERT INTO dog_options VALUES
+('Darla', 250, 7, 'Golden Retriever', 'F', 'Temperamental but great with adults');
+
+INSERT INTO dog_options VALUES
+('Sweeny', 400, 1, 'Bichon Frise Mix', 'M', 'Fantastic with all ages');
+
+INSERT INTO dog_options VALUES
+('Caty', 260, 6, 'Unknown', 'F', 'Poor Training, but kind');
+
+INSERT INTO dog_options VALUES
+('Fido', 360.99, 3, 'Sausage Dog Mix','M', 'Snippy');
+
+INSERT INTO dog_options VALUES       
+('Daryl', 300, 6, 'Unknown', 'M', 'Loud Personality, great with kids & babies);
+
+INSERT INTO dog_options VALUES
+('Charlie', 360, 4, 'Poodle Mix', 'M', 'Polite and Well trained');         
+
