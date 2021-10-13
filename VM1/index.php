f43d9d7d4258c6b855a0d39a1bb99c612c6c2cb4
fkraayvanger <89968563+fkraayvanger@users.noreply.github.com>
2021-10-13 14:06:05 +1300

    Slight changes

--- VM1/index.php
+++ VM1/index.php
@@ -1,55 +1,53 @@
-<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
 <html>
   <head>
     <title>Dog Museum</title>
   </head>
   <body>
     <div class="space">
-        <div style="padding: 60px; text-align: center;background-color: #FAAFBE; border-color: #FAAFBE;">
-          <div style="color: #F52887; font-family: Tahoma, sans-serif;"><h1> Dog Museum </h1></div>
-          <div style="color: #FF69B4; font-family: Tahoma, sans-serif;"><h2>Welcome to the Dog Museum</h2></div>
-       </div>
+      <div style="padding: 60px; text-align: center;background-color: #FAAFBE; border-color: #FAAFBE;"> 
+        <div style="color: #F52887; font-family: Tahoma, sans-serif;"><h1> Dog Museum </h1></div>
+        <div style="color: #FF69B4; font-family: Tahoma, sans-serif;"><h2>Welcome to the Dog Museum</h2></div>
+      </div>
     </div>
   </body>
   <div class="topnav">
     <a class="active" href="index.php">Home</a>
-    <a class="active" href="dogs.php">Dogs</a>
+    <a class="active" href="dogs.php">Dogs</a>
   </div>
   <div style="max-width: 500px; height: 600px; background-color: #FAAFBE; margin: auto; border-color:#FAAFBE;">
-      <body>
-        <div style="font-family: Tahoma, sans-serif; margin-right: 30px; margin-left: 30px; height 600px; background-color: #FFC0CB; color: #F52887">
-    <center><h3> The Dog Museum brings in a new way to adopt, with the ease of a click you can easily begin your journey to owning your own adopted pup.</h3></center>
-    </div>
-    <img class="image1" src="freeimage.jpg" alt="dog" width ="300" height="300">
-    <div style="font-family: Tahoma, sans-serif; margin-right: 30px; margin-left: 30px; height 600px; background-color: #FFC0CB; color: #F52887">
-    <center><h3> Our Company prides itself on the effort to help abandoned dogs and pups find their forever home</h3></center>
-    </div>
+    <body>
+      <div style="font-family: Tahoma, sans-serif; margin-right: 30px; margin-left: 30px; height 600px; background-color: #FFC0CB; color: #F52887">
+        <center><h3> The Dog Museum brings in a new way to adopt, with the ease of a click you can easily begin your journey to owning your own adopted pup.</h3></center>
+      </div>
+      <img class="image1" src="https://cdn.pixabay.com/photo/2018/05/26/18/06/dog-3431913_1280.jpg" alt="dog" width ="300" height="300">
+      <div style="font-family: Tahoma, sans-serif; margin-right: 30px; margin-left: 30px; height 600px; background-color: #FFC0CB; color: #F52887">
+        <center><h3> Our Company prides itself on the effort to help abandoned dogs and pups find their forever home</h3></center>
+      </div>
     </body>
-</div>
-
-</div>
+  </div>
   <center><h3> All images are royalty-free from: pixabay.com</h3></center>
 </html>
 <style>
-.space {
+  .space {
   max-width: 500px;
   margin: auto;
   text-align: center;
-}
-.image1{
+  }
+  .image1{
   margin: 10px auto 20px;
   display: block;
-}
-.topnav {
-width: 750px;
-margin:0 auto;
-position: relative;
+  }
+  .topnav {
+  width: 750px;
+  margin:0 auto;
+  position: relative;
   background-color: #F52887;
   overflow: hidden;
   border-color: #F52887;
-}
+  }
 
-.topnav a {
+  .topnav a {
   width: 150px;
   float: left;
   color: #FAAFBE;
@@ -57,15 +55,15 @@ position: relative;
   padding: 14px 16px;
   text-decoration: none;
   font-size: 17px;
-}
+  }
 
-.topnav a:hover {
+  .topnav a:hover {
   background-color: #E38AAE;
   color: black;
-}
+  }
 
-.topnav a.active {
+  .topnav a.active {
   background-color: #E38AAE;
   color: white;
-}
-</style>
+  }
+</style>
